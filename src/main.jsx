5c1a4499f54d8faa09a6c31ebaa8d453b8badfa6
Tesha R <tesha@ldrdesigns.com>
2022-10-21 20:27:20 -0700

    Removed routing until URL is fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Root from './Root';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+//import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <App />,
-  },
-  {
-    path: 'App',
-    element: <App />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//   },
+//   {
+//     path: 'App',
+//     element: <App />,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
